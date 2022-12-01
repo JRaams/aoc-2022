148a7745c8fb1072862ccde3e3afbd740ece5cb0
JRaams <47593569+JRaams@users.noreply.github.com>
2022-12-01 19:50:03 +0100

    simplify 01

--- 01/b.ts
+++ 01/b.ts
@@ -1,13 +1,11 @@
-import { loadElves } from "./elf.ts";
-
 const lines: string = await Deno.readTextFile("./input.txt");
-const input: string[] = lines.split("\n");
+const input: number[] = lines
+  .split("\n\n")
+  .map((s) => s.split("\n").map(Number).reduce((a, b) => a + b, 0));
 
-function solve(lines: string[]): number {
-  const elves = loadElves(lines);
-  elves.sort((a, b) => a > b ? -1 : 1);
-  return elves[0] + elves[1] + elves[2];
+function solve(input: number[]): number {
+  input.sort((a, b) => a > b ? -1 : 1);
+  return input[0] + input[1] + input[2];
 }
 
-const result = solve(input);
-console.info(result);
+console.info(solve(input));
