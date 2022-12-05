174fcfae754f04dfce792d232036b1213c361871
JRaams <47593569+JRaams@users.noreply.github.com>
2022-12-05 19:55:39 +0100

    simplify 04b

--- 04/b.ts
+++ 04/b.ts
@@ -1,18 +1,6 @@
 const lines: string = await Deno.readTextFile("./input.txt");
 const input: string[] = lines.split("\n").filter((l) => l);
 
-function hasOverlap(a1: number, a2: number, b1: number, b2: number): boolean {
-  for (let i = a1; i <= a2; i++) {
-    for (let j = b1; j <= b2; j++) {
-      if (i === j) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-}
-
 function solve(lines: string[]): number {
   let count = 0;
 
@@ -21,7 +9,7 @@ function solve(lines: string[]): number {
     const [a1, a2] = elfA.split("-").map(Number);
     const [b1, b2] = elfB.split("-").map(Number);
 
-    if (hasOverlap(a1, a2, b1, b2)) {
+    if (a2 >= b1 && b2 >= a1) {
       count++;
     }
   });
