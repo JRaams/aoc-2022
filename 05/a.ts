21484bea9e42f8b6a072e17e54e7b5be95dd22a9
JRaams <47593569+JRaams@users.noreply.github.com>
2022-12-05 19:41:06 +0100

    05b

--- 05/a.ts
+++ 05/a.ts
@@ -4,7 +4,7 @@ const [cratesStr, procedureStr] = lines.split("\n\n");
 export function loadCrates(cratesStr: string): string[][] {
   const crates: string[][] = [];
 
-  // Load stacks of crates
+  // Create space (array) for each stack of crates
   const lines = cratesStr.split("\n");
   lines[lines.length - 1].split("  ").map((s) => Number(s.trim())).forEach(() =>
     crates.push([])
@@ -12,17 +12,13 @@ export function loadCrates(cratesStr: string): string[][] {
 
   // Fill each crate from the bottom up
   for (let i = lines.length - 2; i >= 0; i--) {
-    const chars: string[] = [];
     const line = lines[i];
     for (let k = 1; k < line.length; k += 4) {
       const char = line[k];
-      chars.push(char);
-    }
-
-    for (let j = 0; j < chars.length; j++) {
-      const char = chars[j];
       if (char === " ") continue;
-      crates[j].push(char);
+
+      const index = Math.floor((k - 1) / 4);
+      crates[index].push(char);
     }
   }
 
@@ -31,16 +27,13 @@ export function loadCrates(cratesStr: string): string[][] {
 
 export function solve(crates: string[][], procedure: string): string {
   procedure.split("\n").forEach((line) => {
-    const [count, from, to] = line.split(" ").filter((s) => !isNaN(s)).map(
-      Number,
+    const [count, from, to] = line.split(" ").map(Number).filter((s) =>
+      !isNaN(s)
     );
 
     for (let i = 0; i < count; i++) {
       const crate = crates[from - 1].pop();
-      if (crate === undefined) {
-        throw new Error("ree");
-      }
-      crates[to - 1].push(crate);
+      crates[to - 1].push(crate!);
     }
   });
 
--- 05/b.ts
+++ 05/b.ts
@@ -1,3 +1,49 @@
 const lines: string = await Deno.readTextFile("./input.txt");
-const input: number[] = lines.split("\n").filter((l) => l).map(Number);
+const [cratesStr, procedureStr] = lines.split("\n\n");
 
+export function loadCrates(cratesStr: string): string[][] {
+  const crates: string[][] = [];
+
+  // Create space (array) for each stack of crates
+  const lines = cratesStr.split("\n");
+  lines[lines.length - 1].split("  ").map((s) => Number(s.trim())).forEach(() =>
+    crates.push([])
+  );
+
+  // Fill each crate from the bottom up
+  for (let i = lines.length - 2; i >= 0; i--) {
+    const line = lines[i];
+    for (let k = 1; k < line.length; k += 4) {
+      const char = line[k];
+      if (char === " ") continue;
+
+      const index = Math.floor((k - 1) / 4);
+      crates[index].push(char);
+    }
+  }
+
+  return crates;
+}
+
+export function solve(crates: string[][], procedure: string): string {
+  procedure.split("\n").filter((l) => l).forEach((line) => {
+    const [count, from, to] = line.split(" ").map(Number).filter((s) =>
+      !isNaN(s)
+    );
+
+    const cratesToMove = crates[from - 1].splice(
+      crates[from - 1].length - count,
+      count,
+    );
+    crates[to - 1].push(...cratesToMove);
+  });
+
+  return crates.reduce(
+    (prev: string, curr: string[]) => prev + curr[curr.length - 1],
+    "",
+  );
+}
+
+const crates = loadCrates(cratesStr);
+
+console.info(solve(crates, procedureStr));
