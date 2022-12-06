def63191d8fd9cf7fb4809e9c9a5ca56782461c2
JRaams <47593569+JRaams@users.noreply.github.com>
2022-12-06 08:32:27 +0100

    06b

--- 06/b.ts
+++ 06/b.ts
@@ -1,3 +1,18 @@
 const lines: string = await Deno.readTextFile("./input.txt");
-const input: number[] = lines.split("\n").filter((l) => l).map(Number);
+const input: string = lines.split("\n").filter((l) => l)[0];
 
+export function solve(line: string): number {
+  const chars = line.split("");
+
+  for (let i = 13; i < chars.length; i++) {
+    const window = chars.slice(i - 13, i + 1);
+    const set = new Set(window);
+    if (set.size === 14) {
+      return i + 1;
+    }
+  }
+
+  return -1;
+}
+
+console.info(solve(input));
