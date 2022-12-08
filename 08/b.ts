4aeb667d71f1589e27a350663ef0255400f6b541
JRaams <47593569+JRaams@users.noreply.github.com>
2022-12-08 19:37:11 +0100

    cleanup 08b

--- 08/b.ts
+++ 08/b.ts
@@ -4,32 +4,31 @@ const input: number[][] = lines.split("\n").filter((l) => l).map((line) =>
 );
 
 function getScenicScore(
+  forest: number[][],
   y: number,
   x: number,
-  row: number[],
-  col: number[],
 ): number {
   let scoreTop = 0;
   let scoreRight = 0;
   let scoreBottom = 0;
   let scoreLeft = 0;
 
-  const height = row[x];
+  const height = forest[y][x];
   for (let j = y - 1; j >= 0; j--) {
     scoreTop++;
-    if (col[j] >= height) break;
+    if (forest[j][x] >= height) break;
   }
-  for (let i = x + 1; i < row.length; i++) {
+  for (let i = x + 1; i < forest[y].length; i++) {
     scoreRight++;
-    if (row[i] >= height) break;
+    if (forest[y][i] >= height) break;
   }
-  for (let j = y + 1; j < col.length; j++) {
+  for (let j = y + 1; j < forest.length; j++) {
     scoreBottom++;
-    if (col[j] >= height) break;
+    if (forest[j][x] >= height) break;
   }
   for (let i = x - 1; i >= 0; i--) {
     scoreLeft++;
-    if (row[i] >= height) break;
+    if (forest[y][i] >= height) break;
   }
 
   return scoreTop * scoreRight * scoreBottom * scoreLeft;
@@ -39,12 +38,8 @@ function solve(forest: number[][]): number {
   let result = 0;
 
   for (let y = 1; y < forest.length - 1; y++) {
-    const row = forest[y];
-
-    for (let x = 1; x < row.length - 1; x++) {
-      const col = forest.map((r) => r[x]);
-
-      const score = getScenicScore(y, x, row, col);
+    for (let x = 1; x < forest[y].length - 1; x++) {
+      const score = getScenicScore(forest, y, x);
       if (score > result) {
         result = score;
       }
