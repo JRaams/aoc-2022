22eb428aee04bde8377e8bc650239d5d7b5de443
JRaams <47593569+JRaams@users.noreply.github.com>
2022-12-10 13:43:47 +0100

    10b

--- 10/b.ts
+++ 10/b.ts
@@ -1,3 +1,25 @@
-const lines: string = await Deno.readTextFile("./input.txt");
-const input: number[] = lines.split("\n").filter((l) => l).map(Number);
+const line: string = await Deno.readTextFile("./input.txt");
+const input: string[] = line.replaceAll("addx", "noop\naddx")
+  .split("\n").filter((l) => l);
 
+export function solve(i: string[]): void {
+  let X = 1;
+  const pixels: string[] = [];
+
+  i.forEach((line, index) => {
+    const indexInRow = index % 40;
+    const shouldDrawPixel = Math.abs(indexInRow - X) <= 1;
+    pixels.push(shouldDrawPixel ? "#" : " ");
+
+    const [instruction, valueStr] = line.split(" ");
+    if (instruction === "addx") {
+      X += Number(valueStr);
+    }
+  });
+
+  for (let i = 0; i < 6; i++) {
+    console.info(pixels.slice(40 * i, 40 * i + 40).join(""));
+  }
+}
+
+solve(input);
