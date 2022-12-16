3731f4aecdff50342006acf75c2e0f467f68c458
JRaams <47593569+JRaams@users.noreply.github.com>
2022-12-16 11:58:22 +0100

    refactor 15b

--- 15/b.ts
+++ 15/b.ts
@@ -6,42 +6,34 @@ function isDetected(sensors: Sensor[], pos: Pos): boolean {
   });
 }
 
-function solve(): number {
+function solve(LIMIT: number): number {
   const sensors: Sensor[] = loadMap().map((l) => l.sensor);
-  const LIMIT = 4_000_000;
 
   for (let i = 0; i < sensors.length; i++) {
     const sensor = sensors[i];
-    let dx = 0;
-    const minY = Math.max(sensor.y - sensor.radius + 1, 0);
+    const minY = Math.max(sensor.y - sensor.radius - 1, 0);
     const maxY = Math.min(sensor.y + sensor.radius + 1, LIMIT);
 
     // Walk around every sensors edges (radius + 1) to see if the positions are detected by other sensors
+    let dx = 0;
     for (let dy = minY; dy <= maxY; dy++) {
-      const edges: Pos[] = dx === 0 ? [{ x: sensor.x, y: dy }] : [
+      const edges: Pos[] = [
         { x: sensor.x + dx, y: dy },
         { x: sensor.x - dx, y: dy },
       ];
 
-      const isolatedPos = edges.filter(({ x, y }) =>
-        x >= 0 && x <= LIMIT && y >= 0 && y <= LIMIT
-      ).find((
-        pos,
-      ) => !isDetected(sensors, pos));
+      const isolatedPos = edges.filter(({ x }) => x >= 0 && x <= LIMIT)
+        .find((pos) => !isDetected(sensors, pos));
 
       if (isolatedPos) {
         return isolatedPos.x * 4_000_000 + isolatedPos.y;
       }
 
-      if (dy <= sensor.y) {
-        dx++;
-      } else {
-        dx--;
-      }
+      dx++;
     }
   }
 
   return -1;
 }
 
-console.info(solve());
+console.info(solve(4_000_000));
